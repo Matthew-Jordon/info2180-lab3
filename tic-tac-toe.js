0d29e4511285b2daa138574033d9d326720a3b4c
Matthew-Jordon Whynter <mattwhynt@yahoo.com>
2020-10-08 00:13:01 -0500

    added styling for when a box is clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,16 @@ window.onload = function () {
     for (let index = 0; index < 9; index++) {
         let count = 0;
         board.children.item(index).classList.add("square");
+        board.children.item(index).addEventListener("click", function(){
+            if ((turns %2) == 0) {
+                board.children.item(index).innerHTML = "X";
+                board.children.item(index).classList.add("X");
+            } else {
+                board.children.item(index).innerHTML = "O";
+                board.children.item(index).classList.add("O");
+            }
+            turns++;
+        });
     }
 }
 
