43904b8f4e5a07bc4e65faea9e692bbb3b6d2990
Matthew-Jordon Whynter <mattwhynt@yahoo.com>
2020-10-11 19:18:58 -0500

    added the functionality to declare a winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,7 @@
 var board;
 var turns =0;
+var info = [];
+var ngame;
 window.onload = function () {
     board = document.getElementById("board");
     for (let index = 0; index < 9; index++) {
@@ -9,10 +11,13 @@ window.onload = function () {
             if ((turns %2) == 0) {
                 board.children.item(index).innerHTML = "X";
                 board.children.item(index).classList.add("X");
+                info[index] = "X";
             } else {
                 board.children.item(index).innerHTML = "O";
                 board.children.item(index).classList.add("O");
+                info[index] = "O";
             }
+            check();
             turns++;
         });
         board.children.item(index).onmouseenter = function() {
@@ -21,5 +26,32 @@ window.onload = function () {
         board.children.item(index).onmouseleave = function() {
             board.children.item(index).classList.remove("hover");
         }
+    }
+}
+function check() {
+    if (info[0] == info[1] && info[0] == info[2] && info[0] != undefined) {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! " + info[0] + " is the Winner"; 
+    } if (info[3] == info[4] && info[3] == info[5] && info[3] != undefined) {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! " + info[3] + " is the Winner";
+    } if (info[6] == info[7] && info[6] == info[8] && info[6] != undefined) {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! " + info[6] + " is the Winner";
+    } if (info[0] == info[3] && info[0] == info[6] && info[0] != undefined) {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! " + info[0] + " is the Winner";
+    } if (info[1] == info[4] && info[1] == info[7] && info[1] != undefined) {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! " + info[1] + " is the Winner";
+    } if (info[2] == info[5] && info[2] == info[8] && info[2] != undefined) {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! " + info[2] + " is the Winner";
+    } if (info[0] == info[4] && info[0] == info[8] && info[0] != undefined) {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! " + info[0] + " is the Winner";
+    } if (info[2] == info[4] && info[2] == info[6] && info[2] != undefined) {
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").innerHTML = "Congratulations! " + info[2] + " is the Winner";
     }
 }
